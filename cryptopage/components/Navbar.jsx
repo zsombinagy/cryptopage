155f358a67c364ba75a71b9b4963a83a6e37a91e
iamzsombi <zsombinagy.14@gmail.com>
2022-01-25 21:07:12 +0100

    keine angst haben

--- cryptopage/components/Navbar.jsx
+++ cryptopage/components/Navbar.jsx
@@ -1,35 +1,34 @@
-import React from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
-import { Link } from "react-router-dom";
+import Link from "next/link"
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from "@ant-design/icons";
+import icon from "../public/cryptocurrency.png"
 
-import icon from "../images/cryptocurrency.png";
 
 const Navbar = () => {
   return (
-        <div className="nav-container">
-            <div className="logo-container">
-                <Avatar src={icon} size="large" />
-                <Typography.Title level={2} className='logo' >
-                    <Link to="/">Cryptoverse</Link>
-                </Typography.Title>
-                <Menu theme="dark">
-                    <Menu.Item icon={<HomeOutlined />}>
-                        <Link to="/">Home</Link>
-                    </Menu.Item>
-                    <Menu.Item icon={<FundOutlined />}>
-                        <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-                    </Menu.Item>
-                    <Menu.Item icon={<MoneyCollectOutlined />}>
-                        <Link to="/exchanges">Exchanges</Link>
-                    </Menu.Item>
-                    <Menu.Item icon={<BulbOutlined />}>
-                        <Link to="/news">News</Link>
-                    </Menu.Item>
-                </Menu>
+    <div className="nav-container">
+    <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className='logo' >
+            <Link href="/">Cryptoverse</Link>
+        </Typography.Title>
+        <Menu theme="dark">
+            <Menu.Item icon={<HomeOutlined />}>
+                <Link href="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined />}>
+                <Link href="/cryptocurrencies">Cryptocurrencies</Link>
+            </Menu.Item>
+            <Menu.Item icon={<MoneyCollectOutlined />}>
+                <Link href="/exchanges">Exchanges</Link>
+            </Menu.Item>
+            <Menu.Item icon={<BulbOutlined />}>
+                <Link href="/news">News</Link>
+            </Menu.Item>
+        </Menu>
 
-            </div>
-        </div>
+    </div>
+</div>
     )
 };
 
